79d11c294fbc7163eed5dbd7da58d4a2431d69e6
Hitartha <hitartha@theqa.in>
2022-10-21 17:03:45 +0530

    Webpack Ready
    * SASS/SCSS compile to css
    * All JavaScript file combile to single file with babel

---
+++ README.md
@@ -0,0 +1,32 @@
+## Webpack 5
+
+**Preparations:** [Install node.js](https://docs.understrap.com/#/understrap-child/npm?id=preparations-install-nodejs)
+
+At first you need node.js installed on your computer globally. If you already done this before skip this section. If not: You have to install node.js (comes along with npm) before you can proceed. To install node.js visit the node.js website for the latest installer for your OS. Download and install it like any other program, too.
+
+We also recommend using [NVM - Node Version Manager](https://github.com/nvm-sh/nvm) to keep closer control over your version of node.js and switch between versions.
+
+**Installing dependencies**
+
+* Make sure you have installed Node.js on your computer globally
+* Then open your terminal and browse to the location of your projects
+* Run: `npm install`
+
+**Running Project** 
+
+To work and compile your Sass/JavaScript files on the fly start:
+
+```
+npm start
+```
+**Build Project**
+
+For development
+```
+npm run dev
+```
+
+For production
+```
+npm run build
+```
---
+++ src/build/webpack.config.js
@@ -0,0 +1,102 @@
+const path = require("path");
+const webpack = require("webpack");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const RemovePlugin = require("remove-files-webpack-plugin");
+
+var config = {
+	entry: {
+		// Define the entry points of our application (can be multiple for different sections of a website)
+		"theme-name": ["./src/assets/sass/main.scss", "./src/assets/js/main.js"],
+	},
+	output: {
+		// Define the destination directory and filenames of compiled resources
+		filename: "js/[name].min.js",
+		path: path.resolve(__dirname, "./../../assets"),
+	},
+	module: {
+		rules: [
+			{
+				// This rule is here to Use babel for JS files
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
+				},
+			},
+			{
+				// This rule is here to Exposes jQuery for use outside of Webpack build
+				test: require.resolve("jquery"),
+				loader: "expose-loader",
+				options: {
+					exposes: ["$", "jQuery"],
+				},
+			},
+			{
+				// This rule is here for compile SASS/SCSS to css
+				test: /\.s[ac]ss$/,
+				use: [
+					MiniCssExtractPlugin.loader, // instead of style-loader
+					{ loader: "css-loader", options: { sourceMap: true } },
+					{ loader: "postcss-loader", options: { sourceMap: true } },
+					{ loader: "sass-loader", options: { sourceMap: true } },
+				],
+			},
+			{
+				//This rule is here to include font awesome deps as separate font
+				test: /\.(svg|eot|woff|woff2|ttf)$/,
+				type: "asset/resource",
+				generator: {
+					//publicPath: '../fonts/',
+					filename: "./assets/fonts/[hash][ext][query]",
+				},
+			},
+		],
+	},
+	optimization: {},
+	plugins: [
+		new RemovePlugin({
+			// Removes files and folders before and after compilation
+			before: {
+				// Before compilation permanently removes entire './assets' folder.
+				include: ["./assets"],
+			},
+			watch: {
+				// parameters for "before watch compilation" stage.
+			},
+			after: {
+				// parameters for "after normal and watch compilation" stage.
+			},
+		}),
+		new MiniCssExtractPlugin({
+			// Extracts CSS into separate files
+			filename: "css/[name].min.css",
+			chunkFilename: "[id].css",
+		}),
+		new CopyPlugin({
+			patterns: [{ from: "./src/assets/images/", to: "./images" }],
+		}),
+	],
+};
+
+module.exports = (env, argv) => {
+	if (argv.mode === "development") {
+		// only for development mode
+		config.devtool = "source-map"; // Define development options
+	}
+
+	if (argv.mode === "production") {
+		// only for production mode
+		config.optimization = {
+			minimize: true,
+			minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+		};
+	}
+
+	return config;
+};
